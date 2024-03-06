45abd0f1bee04b8d7f216437db78e2b5bd9fdc2b
Nurmira16 <nurmirajorobek@gmail.com>
2024-03-06 15:21:24 +0100

    Update

--- js/main.js
+++ js/main.js
@@ -17,3 +17,41 @@ const contactForm=()=>{
     form.style.display=(form.style.display=='flex')?'none':'flex'
     
 }
+
+window.addEventListener('scroll', function() {
+    var scrollingDiv = document.getElementById('about');
+    var scrollPosition = window.scrollY;
+
+    // Adjust the value (200 in this example) to your desired scroll threshold
+    if (scrollPosition > 675) {
+        scrollingDiv.classList.add('visible');
+    } else {
+        scrollingDiv.classList.remove('visible');
+    }
+});
+window.addEventListener('scroll', function() {
+    var scrollingDiv = document.getElementById('projects');
+    var scrollPosition = window.scrollY;
+
+    // Adjust the value (200 in this example) to your desired scroll threshold
+    if (scrollPosition > 1970) {
+        scrollingDiv.classList.add('visible');
+    } else {
+        scrollingDiv.classList.remove('visible');
+    }
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Hide loading page after 3 seconds
+    setTimeout(function() {
+        var loadingPage = document.getElementById('loadingPage');
+        var content = document.getElementById('content');
+
+        loadingPage.style.opacity = '0';
+        content.style.opacity = '1';
+        loadingPage.style.minHeight='0'
+
+        // Show scrollbars after loading
+        document.body.style.overflow = 'visible';
+    },2000);
+});
+console.log(window.scrollY)
